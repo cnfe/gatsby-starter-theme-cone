537b4a86a737e6e12dc8379414802a803767ba82
影观 <ximing.huxm@alibaba-inc.com>
2020-07-10 12:09:13 +0800

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,37 +4,39 @@ const siteConfig = require('./config.json');
 exports.onCreateWebpackConfig = ({ stage, getConfig }) => {
   const config = getConfig();
 
-  if (stage === 'build-javascript') {
-    config.output.filename = '[name].js';
-    config.output.chunkFilename = '[name].js';
-  }
-
-  for (let i = 0, len = config.plugins.length; i < len; i++) {
-    const plugin = config.plugins[i];
-    if (plugin instanceof MiniCssExtractPlugin) {
-      config.plugins.splice(i, 1);
-      break;
+  if (stage.startsWith('build')) {
+    if (stage === 'build-javascript') {
+      config.output.filename = '[name].js';
+      config.output.chunkFilename = '[name].js';
     }
-  }
 
-  // 添加规则
-  config.module.rules.unshift({
-    test: /\.js?$/,
-    use: {
-      loader: 'string-replace-loader',
-      options: {
-        search: /\/page-data\//g,
-        replace: `/${siteConfig.deploy.version}/page-data/`
+    for (let i = 0, len = config.plugins.length; i < len; i++) {
+      const plugin = config.plugins[i];
+      if (plugin instanceof MiniCssExtractPlugin) {
+        config.plugins.splice(i, 1);
+        break;
       }
     }
-  });
 
-  config.plugins.push(
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[name].css',
-      ignoreOrder: false,
-      moduleFilename: () => this.options.filename || DEFAULT_FILENAME
-    })
-  );
+    // 添加规则
+    config.module.rules.unshift({
+      test: /\.js?$/,
+      use: {
+        loader: 'string-replace-loader',
+        options: {
+          search: /\/page-data\//g,
+          replace: `/${siteConfig.deploy.version}/page-data/`
+        }
+      }
+    });
+
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[name].css',
+        ignoreOrder: false,
+        moduleFilename: () => this.options.filename || DEFAULT_FILENAME
+      })
+    );
+  }
 };
